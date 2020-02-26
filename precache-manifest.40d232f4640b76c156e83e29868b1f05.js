f82be8b2612fdff1490a64474415c2e884d12545
Cisino Junior <cisinojr@gmail.com>
2020-02-26 11:51:17 -0300

    Updates

---
+++ precache-manifest.40d232f4640b76c156e83e29868b1f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf23d0040594116b3e753eefdf2c2ab",
+    "url": "/contracts-viewer/index.html"
+  },
+  {
+    "revision": "b82d308f43a55607c854",
+    "url": "/contracts-viewer/static/css/main.68a94bec.chunk.css"
+  },
+  {
+    "revision": "be2c5dcf03017a0f6861",
+    "url": "/contracts-viewer/static/js/2.bd1dc913.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/contracts-viewer/static/js/2.bd1dc913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82d308f43a55607c854",
+    "url": "/contracts-viewer/static/js/main.ed48ed7b.chunk.js"
+  },
+  {
+    "revision": "6fe1534d942b0a4a52e1",
+    "url": "/contracts-viewer/static/js/runtime-main.95d7153b.js"
+  }
+]);
