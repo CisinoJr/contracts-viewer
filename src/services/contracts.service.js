f815f61c4075f92d82c4679fc42fd01ea284d07d
Cisino Junior <cisinojr@gmail.com>
2020-02-26 11:53:47 -0300

    chore: configure github deploy

--- src/services/contracts.service.js
+++ src/services/contracts.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://localhost:3000/`,
+  baseURL: `https://cisinojr.github.io/contracts-viewer`,
 });
 
 export default class ContractsService {
